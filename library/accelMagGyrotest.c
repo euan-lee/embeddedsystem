106d131e26adb73ac10bdb9d0fde5da13555a7e7
yeseohong7 <93835186+yeseohong7@users.noreply.github.com>
2021-12-17 04:46:05 +0900

    library

---
+++ library/accelMagGyrotest.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "accelMagGyro.h"
+
+int main (void)
+{
+	accInit();
+	magInit();
+	gyroInit();
+	
+	getAcc();
+	getMag();
+	getGyro();
+	
+	return 0;
+}
+
